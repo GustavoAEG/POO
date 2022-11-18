dc3f74eda7d1b04c5a75c5d52378bab9826700b3
Gustavo <gustavoenriccoga@gmail.com>
2022-11-18 19:25:38 -0300

    Heranca e construtor

--- trilha-net-poo-desafio-main/Program.cs
+++ trilha-net-poo-desafio-main/Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia Android");
+Nokia android = new Nokia(numero:"123123","A35","2232dsasd2342sda",512);
+android.Ligar();
+android.InstalarAplicativo("WeatherApp");
+
+Console.WriteLine("Iphone X");
+Iphone x = new Iphone(numero:"123123","X","2232dsasd2342sda",1024);
+x.Ligar();
+x.InstalarAplicativo("Icloud");
