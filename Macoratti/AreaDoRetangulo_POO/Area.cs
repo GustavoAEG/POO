3c0e3c6a62c94ea411392228be9c7751234d4ffc
Gustavo <gustavoenriccoga@gmail.com>
2022-11-19 19:40:36 -0300

    Retorno metodo Resultado

---
+++ Macoratti/AreaDoRetangulo_POO/Area.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AreaDoRetangulo_POO
+{
+    public class Area
+    {
+        public Area()
+        {
+
+        }
+        public int altura;
+        public int Altura
+        {
+            get { return altura; }
+            set
+            {
+                if (altura >= 0)
+                {
+                    altura = value;
+                }
+
+
+            }
+        }
+        public int base_;
+
+        public int Base_
+        {
+            get { return base_; }
+            set
+            {
+                if (base_ >= 0)
+                {
+                    base_ = value;
+                }
+            }
+        }
+
+     public double Resultado { get{return base_*altura;}}
+
+        public void Resultado_area()
+        {
+
+            Console.WriteLine("A area total é de " + Resultado + "m²");
+        }
+
+
+    }
+}
