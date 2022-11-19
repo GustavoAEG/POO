933aa54af58a2fcc65d03df60a3a4d02d059d609
Gustavo <gustavoenriccoga@gmail.com>
2022-11-19 20:00:40 -0300

    Finished

---
+++ Macoratti/AreaDoRetangulo_POO/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using AreaDoRetangulo_POO;
+
+Area a1 = new Area();
+Console.WriteLine("Resultado do cálculo da área");
+Console.WriteLine("Informe valor da Base");
+a1.altura= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe valor da altura");
+a1.base_ = Convert.ToInt32(Console.ReadLine());
+
+a1.Resultado_area();
